2bda791856b7967f54861a62f4c41b6b30942bf8
ssalim <salim.skander@ynov.com>
2024-01-24 18:42:33 +0100

    edit all fields user

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from models.User import User
 from flask_pymongo import PyMongo
 from flask import current_app
+from werkzeug.security import generate_password_hash
 from flask_jwt_extended import jwt_required
 
 user_routes = Blueprint('user_routes', __name__, url_prefix='/api/users')
@@ -34,14 +35,29 @@ def get_user(username):
 def update_user(username):
     try:
         data = request.get_json()
-
         existing_user = PyMongo(current_app).db.users.find_one({'username': username})
+
         if not existing_user:
             return jsonify({'message': 'Utilisateur non trouvé'}), 404
 
-        PyMongo(current_app).db.users.update_one({'username': username}, {"$set" : {"username" : data.get('username')}})
+        update_data = {}
+        # Vérifier si le champ 'password' est présent dans la requête
+        if pwd := data.get('password') :
+            update_data['password'] = generate_password_hash(data['password'], method='pbkdf2')
 
-        return jsonify({'message': 'Utilisateur mis à jour avec succès'}), 200
+        
+
+        # Vérifier si le champ 'username' est présent dans la requête
+        if usrname := data.get('username'):
+            update_data['username'] = data['username']
+
+        # Assurez-vous que la clé à mettre à jour existe dans le dictionnaire update_data
+        if update_data:
+            # Mettre à jour l'utilisateur avec les champs appropriés
+            PyMongo(current_app).db.users.update_one({'username': username}, {"$set": update_data})
+            return jsonify({'message': 'Utilisateur mis à jour avec succès'}), 200
+        else:
+            return jsonify({'message': 'Aucune donnée à mettre à jour'}), 400
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
